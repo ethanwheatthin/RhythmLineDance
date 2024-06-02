62c55cb1ac0e27b242557ccbce07de58abe67625
unknown <teamavarice661@gmailcom>
2024-06-01 21:44:38 -0600

    server, sample data, search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MaterialModule } from 'src/material.module';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { SearchComponent } from './components/search/search/search.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
-    
+    HeaderComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    AutoCompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
